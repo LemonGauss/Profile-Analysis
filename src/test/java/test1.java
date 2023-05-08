2d164349bb862ab40b2e044681ca68a9fdd0cd77
LemonGauss <2425924636@qq.com>
2023-05-08 19:58:45 +0800

    。

---
+++ src/test/java/test1.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml" })
+public class test1 {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public  void test01() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println("------------------------------------------------------");
+        System.out.println(connection);
+    }
+
+
+
+
+}
